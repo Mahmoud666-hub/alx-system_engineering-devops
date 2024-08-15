de17805dc7ab35c94fcac0aa26c2772133b49d3c
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-15 13:48:12 +0300

    011

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,13 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Custom"}
     resp = requests.get(url, headers=headers, allow_redirects=False)
-    if resp.status_code != 200:
-        return 0
-    else:
-        return resp.json()["data"]["subscribers"]
+    # if resp.status_code != 200:
+    #     return 0
+    # else:
+    #     return resp.json()["data"]["subscribers"]
+    if resp.status_code == 200:
+        try:
+            return resp.json()['data']['subscribers']
+        except KeyError:
+            pass
+    return 0
