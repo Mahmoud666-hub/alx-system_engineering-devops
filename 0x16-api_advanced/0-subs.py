acceddf1039a95c4e46d45fe411e06fcdcf38e2f
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-15 13:17:21 +0300

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and
+returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """no. of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code != 200:
+        return 0
+    else:
+        return resp.json()["data"]["subscribers"]
