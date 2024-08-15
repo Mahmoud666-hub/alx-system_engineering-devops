587505e81eb48418421cd694b5546e93f0f3100f
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-15 13:42:12 +0300

    011

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,11 @@ def top_ten(subreddit):
     headers = {"User-Agent": "Custom"}
     resp = requests.get(url, headers=headers)
 
-    if resp.status_code != 200:
-        print(None)
-    else:
-        fil = resp.json()["data"]["children"]
+    if resp.status_code == 200:
+        try:
+            fil = resp.json()['data']['children']
+        except KeyError:
+            print("None")
+            return
         for post in fil:
             print(post["data"]["title"])
