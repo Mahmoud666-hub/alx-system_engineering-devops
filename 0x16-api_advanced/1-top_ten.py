52c1aae2920f1c78c521eec9d312237f7dfc2b55
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-15 13:36:36 +0300

    01

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""prints the titles of the first 10 hot posts"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """titles"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Custom"}
+    resp = requests.get(url, headers=headers)
+
+    if resp.status_code != 200:
+        print(None)
+    else:
+        fil = resp.json()["data"]["children"]
+        for post in fil:
+            print(post["data"]["title"])
