89b7ba20b0c15231684b65453fe69ccb03658fcf
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-23 08:00:02 +0300

    1o

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,12 @@
 #fix that make 2000 requests, 943 requests failed
-# Ensure the ULIMIT is set correctly
-file_line { 'set_ulimit_for_nginx':
-  path = > '/etc/default/nginx',
-  line = > 'ULIMIT="-n 4096"',
-  match= > '^ULIMIT=',
-  notify => Exec['restart_nginx'],  # Restart Nginx only if this line is changed
+exec { 'modify_max_open_files_limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  unless  => 'grep -q "max open files limit = 10000" /etc/default/nginx',  # Check if the limit is already set
+  notify  => Exec['restart_nginx'],  # Notify restart if the file is modified
 }
 
-# Restart nginx if modified
+# Restart Nginx service
 exec { 'restart_nginx':
   command     => 'service nginx restart',
   refreshonly => true,  # Only restart if notified by a change
