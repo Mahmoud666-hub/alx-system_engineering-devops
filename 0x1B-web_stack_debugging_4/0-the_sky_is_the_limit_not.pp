816d473849fd9fce9e77e82f36f51eca02c83575
mahmoud <ramadanmahmoud945@gmail.com>
2024-08-23 08:44:06 +0300

    1o1d

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,10 @@
 #fix that make 2000 requests, 943 requests failed
-sed -i 's/15/1000/' /etc/default/nginx
-service nginx restart
+exec { 'up_nginx_conf':
+  command => "/usr/bin/env sed -i 's/15/1000/' /etc/default/nginx",
+  notify  => Service['nginx'],
+}
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['up_nginx_conf'],
+}
